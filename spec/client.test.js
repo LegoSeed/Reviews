b61b5b635392397601637b053b2940b6ebf40ccb
Eric Hyde <hydestuf@gmail.com>
2021-01-20 13:14:43 -0600

    Unit Testing Dom Elements

--- spec/client.test.js
+++ spec/client.test.js
@@ -1,23 +1,35 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+import Rating from '../client/components/reviewMod/star_rating';
+import StarRatings from '../client/components/reviewMod/star_rating';
+import Experience from '../client/components/reviewMod/experience';
 import Review from '../client/components/reviewMod/review';
-import Helpful from '../client/components/reviewMod/helpful';
+import 'regenerator-runtime/runtime';
 
-function sum(a, b) {
-  return a + b;
-}
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-//  test components mounting?
-
-describe('<Helpful />', () => {
+describe('<Rating />', () => {
 // add all test blocks here
-  test('renders a Helpful component to the dom', () => {
-    const wrapper = shallow(<Helpful />);
-    expect(wrapper.find()).to.have.lengthOf(0);
+  test('renders a Rating component to the dom', async (done) => {
+    const wrapper = await shallow(<Rating />);
+    expect(wrapper.exists()).toBe(true);
+    done();
   });
+
+  test('renders a StarRatings component to the dom', async (done) => {
+    const wrapper = await shallow(<StarRatings />);
+    expect(wrapper.exists()).toBe(true);
+    done();
+  });
+
+  // test('renders a Review component to the dom', async (done) => {
+  //   const wrapper = await render(<Review />);
+  //   expect(wrapper.exists()).toBe(true);
+  //   done();
+  // });
+
+  // test('renders a Experience component to the dom', async (done) => {
+  //   const wrapper = await mount(<Experience />);
+  //   expect(wrapper.exists()).toBe(true);
+  //   done();
+  // });
 });
