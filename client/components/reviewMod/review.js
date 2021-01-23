2589bd7f522025acb48f368466db22d917e022a0
Eric Hyde <hydestuf@gmail.com>
2021-01-23 10:43:32 -0600

    Formatting and cleanup

--- client/components/reviewMod/review.js
+++ client/components/reviewMod/review.js
@@ -1,46 +1,55 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable no-underscore-dangle */
+
 import React from 'react';
 import moment from 'moment';
-import { Container, Row, Col } from 'react-bootstrap';
+import {
+  Container, Card, Row, Col,
+} from 'react-bootstrap';
 import Rating from './star_rating';
 import Helpful from './helpful';
 import Experience from './experience';
 import Recommend from './recommend';
-// import ReviewBorder from './styles/reviewBorder';
 
 const Review = (allReviews) => (
   <div>
     {allReviews.reviews.data.map((review) => (
-      <Container className="reviewBlock" key={review._id}>
+      <Card className="reviewBlock" key={review._id}>
+        <Container
+          style={{
+            fontSize: '12px',
+            margin: '20px',
+          }}
+        >
 
-        <Row>
-          {moment(review.createdAt).startOf('hour').fromNow()}
-        </Row>
+          <Row>
+            { moment(review.createdAt).startOf('hour').fromNow() }
+          </Row>
 
-        <Row>
-          <Rating rating={review.rating} />
-        </Row>
+          <Row>
+            <Rating rating={review.rating} />
+          </Row>
 
-        <Row className="username">
-          { review.username }
-        </Row>
+          <Row sm={8}>
+            { review.username }
+          </Row>
 
-        <Row>
-          <Recommend review={review} />
-        </Row>
+          <Row>
+            <Recommend review={review} />
+          </Row>
 
-        <Row>
-          <Col sm={8}>{ review.review }</Col>
-          <Col sm={4}>
-            <Experience review={review} />
-          </Col>
-        </Row>
+          <Row>
+            <Col sm={8}>{ review.review }</Col>
+            <Col sm={4}>
+              <Experience review={review} />
+            </Col>
+          </Row>
 
-        <Row>
-          <Helpful review={review} />
-        </Row>
-      </Container>
+          <Row>
+            <Helpful review={review} />
+          </Row>
+        </Container>
+      </Card>
     ))}
   </div>
 );
