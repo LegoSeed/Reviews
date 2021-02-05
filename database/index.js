c85f7fb960fdff5caa0cac635691c0e3b715238e
Randal Schneider <schneider.randal@gmail.com>
2021-02-05 10:45:46 -0600

    Currently creates 2 csv files reviews and productIds

--- database/index.js
+++ database/index.js
@@ -10,14 +10,36 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getReviewsByProductId = (request, response) => {
-  const id = parseInt(request.params.id);
+const getReviewsByProductId = (req, res) => {
+  const id = parseInt(req.params.id);
 
   pool.query('SELECT * FROM reviews WHERE product_id=$1', [id], (error, results) => {
     if (error) {
       throw error;
     }
-    response.status(200).json(results.rows);
+    res.status(200).json(results.rows);
+  });
+};
+
+const postReviews = (req, res) => {
+  const productId = parseInt(req.body.product_id);
+  const userName = req.body.product_name;
+  const { title } = req.body;
+  const { review } = req.body;
+  const rating = parseInt(req.body.rating);
+  const buy = req.body.buy_again;
+  const rec = req.body.would_recommend_to_friend;
+  const play = req.body.play_experience;
+  const diff = req.body.difficulty_level;
+  const value = req.body.value_for_money;
+  const help = req.body.helpful_count;
+  const unHelp = req.body.unhelpful_count;
+  pool.query(`INSERT INTO reviews (product_id, username, title, review, rating, buy_again, would_recommend_to_friend, play_experience, difficulty_level, value_for_money, helpful_count, unhelpful_count)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`, [productId, userName, title, review, rating, buy, rec, play, diff, value, help, unHelp], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(201).send(results);
   });
 };
 
@@ -38,4 +60,5 @@ client.query('SELECT NOW()', (err, res) => {
 
 module.exports = {
   getReviewsByProductId,
+  postReviews,
 };
--- database/schema.sql
+++ database/schema.sql
@@ -5,17 +5,19 @@ CREATE DATABASE sdcproducts;
 -- \l to see created databases
 -- \c to connect to created database
 -- CREATE TABLE
--- CREATE TABLE IF NOT EXISTS products (
---   product_id INT NOT NULL,
---   product_name VARCHAR(50) NOT NULL,
---   PRIMARY KEY(product_id)
--- );
-Drop TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE IF NOT EXISTS products (
+  product_id SERIAL,
+  product_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY(product_id)
+);
+
+DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE IF NOT EXISTS reviews (
-  reviews_id BIGSERIAL PRIMARY KEY,
-  product_id INT NOT NULL,
-  product_name VARCHAR(50) NOT NULL,
+  reviews_id SERIAL PRIMARY KEY,
+  product_id int NOT NULL,
   username VARCHAR(50) NOT NULL,
   title VARCHAR(50) NOT NULL,
   review VARCHAR(600) NOT NULL,
@@ -26,14 +28,14 @@ CREATE TABLE IF NOT EXISTS reviews (
   difficulty_level int NOT NULL,
   value_for_money int NOT NULL,
   helpful_count int NOT NULL,
-  unhelpful_count int NOT NULL
+  unhelpful_count int NOT NULL,
+  CONSTRAINT fk_product
+    FOREIGN KEY(product_id)
+      REFERENCES products(product_id)
 );
 
 -- ADD RECORDS TO TABLE
-INSERT INTO reviews (product_id, product_name, username, title, review, rating, buy_again, would_recommend_to_friend, play_experience, difficulty_level, value_for_money, helpful_count, unhelpful_count) VALUES (
-1, 'Lego truck', 'ryan', 'super awesome', 'lajdsflkjsalkdjf;lasdjflkdsajf', 3, true, true, 3, 4, 5, 43, 22);
+INSERT INTO reviews (product_id, username, title, review, rating, buy_again, would_recommend_to_friend, play_experience, difficulty_level, value_for_money, helpful_count, unhelpful_count) VALUES (
+1, 'ryan', 'super awesome', 'lajdsflkjsalkdjf;lasdjflkdsajf', 3, true, true, 3, 4, 5, 43, 22);
 
--- product_id int NOT NULL,
-  -- CONSTRAINT fk_product
-  --   FOREIGN KEY(product_id)
-  --     REFERENCES products(product_id)
+INSERT INTO products (product_name) VALUES ('JOEY AND THOMAS');
