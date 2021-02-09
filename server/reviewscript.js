d2f55347e90533de2cda53b8a1ca7f17e8ab679b
Randal Schneider <schneider.randal@gmail.com>
2021-02-09 15:57:33 -0600

    Server up on EC2

--- server/reviewscript.js
+++ server/reviewscript.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable func-names */
 /* eslint-disable import/no-unresolved */
 import http from 'k6/http';
@@ -16,3 +17,5 @@ export default function () {
   http.get(`http://0.0.0.0:3002/reviews/${Math.floor(Math.random() * 1000001)}`);
   sleep(1);
 }
+
+// {"product_id": "1001","username": "schneider.randal@yahoo.com","title": "LEARN....","review": "asdfasdfasdfsdfsadfsadf","rating": "3","buy_again": false,"would_recommend_to_friend": true,"play_experience": "5","difficulty_level": "5",  "value_for_money": "5","helpful_count": "75","unhelpful_count": "45"}
